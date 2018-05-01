e977624a9a17f1c9ef0573085c73ef39527b9d9b
Bernard Baltrusaitis <bernard.baltrusaitis@versent.com.au>
2018-05-01 16:47:03 +1000

    wip

--- cmd/yapc/code.go
+++ cmd/yapc/code.go
@@ -3,7 +3,6 @@ package main
 import (
 	"strings"
 	"strconv"
-	"fmt"
 )
 
 package main
@@ -12,61 +11,47 @@ import (
 "fmt"
 "strconv"
 "strings"
+"github.com/imdario/mergo"
 )
 
 func main() {
-	package main
 
-	import (
-		"fmt"
-	"strconv"
-	"strings"
-	)
-
-	func main() {
-		s := map[string]interface{}{
-			"/users/0/name":         "bernard",
-			"/users/0/surname":      "balt",
-			"/users/0/interests/0":  "diy",
-			"/users/0/address/home": "33 creellin",
-			"/users/0/interests/1":  "gym",
-			"/users/1/name":         "keith",
-			"/users/1/surname":      "stubby",
-			"/users/1/interests/0":  "nrl",
-			"/users/1/interests/1":  "v8",
-			"/users/1/address/home": "44 creellin",
+	s := map[string]interface{}{
+		"/users/0/name":         "bernard",
+		"/users/0/surname":      "balt",
+		"/users/0/interests/0":  "diy",
+		"/users/0/address/home": "33 creellin",
+		"/users/0/interests/1":  "gym",
+		"/users/1/name":         "keith",
+		"/users/1/surname":      "stubby",
+		"/users/1/interests/0":  "nrl",
+		"/users/1/interests/1":  "v8",
+		"/users/1/address/home": "44 creellin",
+	}
+	dst := map[string]interface{}{}
+	for k, v := range s {
+		ks := strings.Split(strings.TrimLeft(k, "/"), "/")
+		ks_r := make([]string, len(ks))
+		for ksi, ksv := range ks {
+			ks_r[len(ks)-(ksi+1)] = ksv
 		}
 
-		for k, v := range s {
-			//tr := map[string]interface{}{}
-
-			ks := strings.Split(strings.TrimLeft(k, "/"), "/")
-			ks_r := make([]string, len(ks))
-			for ksi, ksv := range ks {
-				ks_r[len(ks)-(ksi+1)] = ksv
-			}
-
-			_, err := strconv.Atoi(ks_r[0])
+		for _, kv := range ks_r {
+			ik, err := strconv.Atoi(kv)
 			if err == nil {
-				v = []interface{}{v}
-			} else {
-				v = map[string]interface{}{ks_r[0]:v}
+				tmp := make([]interface{}, (ik + 1))
+				tmp[ik] = v
+				v = tmp
+				continue
 			}
 
-			for _, kv := range ks_r[1:] {
-				_, err := strconv.Atoi(kv)
-				if err == nil {
-					v = []interface{}{v}
-					continue
-				}
+			v = map[string]interface{}{kv: v}
 
-				v = map[string]interface{}{kv:v}
-
-
-			}
-			fmt.Println(v)
 		}
 
+		if err := mergo.Merge(&dst, v); err != nil {
+
+		}
 	}
 
 }
