63d91bbb318c0c4f711fa9271addb1315b26df0d
Bernard Baltrusaitis <bernard@runawaylover.info>
2018-05-10 21:48:42 +1000

    wip

--- pkg/source/source_test.go
+++ pkg/source/source_test.go
@@ -1,1 +1,59 @@
 package source_test
+
+import (
+	"io"
+	"testing"
+
+	"os"
+
+	"github.com/b-b3rn4rd/yapc/pkg/source"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSourceJson(t *testing.T) {
+	tests := map[string]struct {
+		r        io.Reader
+		response map[string]interface{}
+		err      error
+	}{
+		"simplemap": {
+			r: func() io.Reader { r, _ := os.Open("testdata/simplemap.json"); return r }(),
+			response: map[string]interface{}{
+				"name":                   "bernard",
+				"address/city":           "melbourne",
+				"address/code":           float64(3000),
+				"address/address/street": "flinders",
+				"address/address/number": float64(1),
+			},
+		},
+		"simpleslice": {
+			r: func() io.Reader { r, _ := os.Open("testdata/simpleslice.json"); return r }(),
+			response: map[string]interface{}{
+				"0/name": "bernard",
+				"1/name": "keith",
+			},
+		},
+		"mapinslice": {
+			r: func() io.Reader { r, _ := os.Open("testdata/mapinslice.json"); return r }(),
+			response: map[string]interface{}{
+				"0/name":     "bernard",
+				"0/colors/0": "red",
+				"0/colors/1": "blue",
+				"1/name":     "keith",
+				"1/colors/0": "black",
+				"1/colors/1": "white",
+			},
+		},
+	}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			s := source.SourceJSON{}
+			r, err := s.Flatten(test.r)
+			assert.Equal(t, test.response, r)
+			if err != nil {
+				assert.Error(t, err, test.err)
+			}
+		})
+	}
+}
--- pkg/storage/storage_test.go
+++ pkg/storage/storage_test.go
@@ -1,1 +1,74 @@
-package storage
+package storage_test
+
+import (
+	"testing"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/ssm"
+	"github.com/b-b3rn4rd/yapc/mocks"
+	"github.com/b-b3rn4rd/yapc/pkg/storage"
+	"github.com/sirupsen/logrus/hooks/test"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+func TestImport(t *testing.T) {
+	values := map[string]interface{}{
+		"0/name":         "bernard",
+		"0/address/work": "1 flinders",
+		"0/address/home": "1 st kilda rd",
+		"1/name":         "keith",
+		"1/address/work": "2 flinders",
+		"1/address/home": "2 st kilda rd",
+	}
+	s := &mocks.SSMAPI{}
+
+	putParameterExpectedInput := map[string]*ssm.PutParameterInput{
+		"/0/name": {
+			Name:      aws.String("/0/name"),
+			Value:     aws.String("bernard"),
+			Type:      aws.String(ssm.ParameterTypeString),
+			Overwrite: aws.Bool(true),
+		},
+		"/0/address/work": {
+			Name:      aws.String("/0/address/work"),
+			Value:     aws.String("1 flinders"),
+			Type:      aws.String(ssm.ParameterTypeString),
+			Overwrite: aws.Bool(true),
+		},
+		"/0/address/home": {
+			Name:      aws.String("/0/address/home"),
+			Value:     aws.String("1 st kilda rd"),
+			Type:      aws.String(ssm.ParameterTypeString),
+			Overwrite: aws.Bool(true),
+		},
+		"/1/name": {
+			Name:      aws.String("/1/name"),
+			Value:     aws.String("keith"),
+			Type:      aws.String(ssm.ParameterTypeString),
+			Overwrite: aws.Bool(true),
+		},
+		"/1/address/work": {
+			Name:      aws.String("/1/address/work"),
+			Value:     aws.String("2 flinders"),
+			Type:      aws.String(ssm.ParameterTypeString),
+			Overwrite: aws.Bool(true),
+		},
+		"/1/address/home": {
+			Name:      aws.String("/1/address/home"),
+			Value:     aws.String("2 st kilda rd"),
+			Type:      aws.String(ssm.ParameterTypeString),
+			Overwrite: aws.Bool(true),
+		},
+	}
+	putParameterExpectedOutput := &ssm.PutParameterOutput{}
+
+	s.On("PutParameter", mock.MatchedBy(func(input *ssm.PutParameterInput) bool {
+		v := putParameterExpectedInput[aws.StringValue(input.Name)]
+		return assert.Equal(t, v, input)
+	})).Times(6).Return(putParameterExpectedOutput, nil)
+
+	logger, _ := test.NewNullLogger()
+	str := storage.New(s, logger)
+	str.Import(values)
+}
